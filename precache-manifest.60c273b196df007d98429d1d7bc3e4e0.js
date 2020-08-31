a226d0e328e3994d655f2f866577ffbf2098df13
liu.xuecong@huadongmedia.com <liu.xuecong@huadongmedia.com>
2020-08-31 12:07:48 +0800

    react index

---
+++ precache-manifest.60c273b196df007d98429d1d7bc3e4e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a5f664ffd31e6d3862a75dff6d1959",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0fcfb0319927dd740de",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c955f629baf16f93c1bc",
+    "url": "/static/js/2.1f23e89c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f23e89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fcfb0319927dd740de",
+    "url": "/static/js/main.0e6b7b90.chunk.js"
+  },
+  {
+    "revision": "e97d2ce558668b25f1a3",
+    "url": "/static/js/runtime-main.78459ccb.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
